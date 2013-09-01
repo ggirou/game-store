778f123e1012b56cdb79a537e52316e14fa8c009
ggirou <porude-git@yahoo.fr>
2013-09-01 18:53:33 +0200

    Add route

--- web/game_store.dart
+++ web/game_store.dart
@@ -1,2 +1,23 @@
+import 'package:route/client.dart';
+
+final homeUrl = new UrlPattern(r'/(.*)#/welcome');
+final gamesUrl = new UrlPattern(r'/(.*)#/games');
+final gameUrl = new UrlPattern(r'/(.*)#/game/(\d+)');
+final allUrls = [homeUrl, gamesUrl, gameUrl];
+
 void main() {
+  var router = new Router()
+    ..addHandler(homeUrl, printIt("Home"))
+    ..addHandler(gamesUrl, printIt("Games"))
+    ..addHandler(gameUrl, showGame)
+    ..listen();
 }
+
+void showGame(String path) {
+  var articleId = gameUrl.parse(path)[1];
+  // show article page with loading indicator
+  // load article from server, then render article
+  print("Game $articleId");
+}
+
+printIt(value) => (_) => print(value);
