a0be44f140dada66738f896972167c7937ffd640
ggirou <porude-git@yahoo.fr>
2013-09-08 21:57:07 +0200

    Update server.dart

--- bin/server.dart
+++ bin/server.dart
@@ -1,18 +1,25 @@
 import 'dart:io';
 import 'packages/path/path.dart' as Path;
 
-startServer(String basePath) {
+startServer(String basePath, String assetPath) {
+  basePath = Path.normalize(basePath);
+  assetPath = Path.normalize(assetPath);
+  
   var address = '127.0.0.1';
   var port = 8080;
   HttpServer.bind(address, port).then((server) {
     log("Server started on http://$address:$port");
-    log("Basepath: $basePath");
+    log("Basepath: $basePath; Assetpath: $assetPath");
     server.listen((HttpRequest request) {
       String path = request.uri.path;
       
-      String stringPath = path == '/' ? '/index.html' : path;
-      File file = new File(Path.normalize('${basePath}${stringPath}'));
-      if(!file.path.startsWith(basePath)) {
+      var isAsset = path.startsWith('/asset/');
+      String filePath = isAsset ? assetPath : basePath;
+      String urlPath = isAsset ? path.replaceFirst("/asset", "") 
+          : (path == '/' ? '/index.html' : path);
+      
+      File file = new File(Path.normalize('${filePath}${urlPath}'));
+      if(!file.path.startsWith(filePath)) {
         _sendNotFound(request, file);
         return;
       }
@@ -41,5 +48,5 @@ main() {
   // Compute base path for the request based on the location of the
   // script and then start the server.
   File script = new File(new File(new Options().script).fullPathSync());
-  startServer(Path.normalize("${script.directory.path}/../web"));
+  startServer("${script.directory.path}/../web", "${script.directory.path}/../asset");
 }
